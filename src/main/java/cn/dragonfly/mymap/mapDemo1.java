bf6a9fea5042d74fab5f7f66445a28bcc7d1d96a
Dragonflyfay <1504195035@qq.com>
2026-02-11 14:08:31 +0800

    第一次提交

---
+++ src/main/java/cn/dragonfly/mymap/mapDemo1.java
@@ -0,0 +1,22 @@
+package cn.dragonfly.mymap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 蜻蜓大王
+ * @date 2026/2/11 1:23
+ * @description map学习
+ */
+public class mapDemo1 {
+    public static void main(String[] args) {
+        //创建Map实现类对象
+        Map<String, String> m=new HashMap<>();
+        m.put("小蜻蜓","18");
+        m.put("小可心","16");
+        m.put("海艳","18");
+        String value = m.put("小可心", "18");
+        System.out.println(value);
+        System.out.println(m);
+    }
+}
